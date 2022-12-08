4c841c786e99f1efd017bbf46f07b5e4fca48588
OneKit <onekit@onekit.cn>
2022-12-08 16:11:39 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,5 @@
 Page({
 	onLoad() {
-		/*
 		var models = [
 			{url:"https://onekit.cn/examples/models/gltf/Soldier.glb",animation:0,scale:1,position:{x:2,y:1,z:0},rotation:{x:0,y:Math.PI,z:0}},
 			{url:"https://onekit.cn/examples/models/gltf/Soldier.glb",animation:1,scale:1,position:{x:0,y:1,z:0},rotation:{x:0,y:Math.PI,z:0}},
@@ -13,19 +12,5 @@ Page({
 			bg,
 			models
 		})
-		*/
-		var urls = [
-			"https://onekit.cn/1/safa.glb",
-			"https://onekit.cn/2/safa.glb"
-	]
-	var images = [
-			"https://onekit.cn/1/safa.jpg",
-			"https://onekit.cn/2/safa.jpg"
-	]
-	var titles = [
-			"棕色温馨",
-			"米色高雅"
-	]
-	this.setData({urls,images,titles})
   }
 })
